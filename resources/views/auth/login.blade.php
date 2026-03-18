7eb00ab1a2dedbe0051e293f94b4a2c70e1ed5dc
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-03-18 19:09:51 +0800

    commit 151

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,30 +6,6 @@
     <h1 class="auth-title">Login</h1>
     <p class="auth-subtitle mb-5">Aplikasi internal. Registrasi publik tidak tersedia.</p>
 
-    @if (session('success'))
-        <div class="alert alert-success" role="alert">
-            {{ session('success') }}
-        </div>
-    @endif
-
-    @if (session('error'))
-        <div class="alert alert-danger" role="alert">
-            {{ session('error') }}
-        </div>
-    @endif
-
-    @if (session('warning'))
-        <div class="alert alert-warning" role="alert">
-            {{ session('warning') }}
-        </div>
-    @endif
-
-    @if (session('info'))
-        <div class="alert alert-info" role="alert">
-            {{ session('info') }}
-        </div>
-    @endif
-
     <form action="{{ route('login.attempt') }}" method="post">
         @csrf
 
