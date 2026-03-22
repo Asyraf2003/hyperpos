0ca4b8af8c145f6fb08a4898460fc53e4a0049cf
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-03-23 01:30:06 +0800

    commit 286

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,9 +14,9 @@ class DatabaseSeeder extends Seeder
             SupplierSeeder::class,
             EmployeeFinanceSeeder::class,
             SupplierInvoiceSeeder::class,
-            SupplierPaymentProofSeeder::class,
             ExpenseSeeder::class,
-            WorkshopStressTestSeeder::class,
+            WorkshopStressTestSeeder::class,        
+            SupplierPaymentProofSeeder::class,
         ]);
     }
 }
