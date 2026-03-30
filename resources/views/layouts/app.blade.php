1bbcb5b21c798b4ee1b4108311b9fb352d83be1e
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-03-31 00:05:17 +0800

    commit 392

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,11 +25,6 @@
         @endif
 
         <div id="main">
-            <header class="mb-3">
-                <a href="#" class="burger-btn d-block d-xl-none">
-                    <i class="bi bi-justify fs-3"></i>
-                </a>
-            </header>
 
             <div class="page-content">
                 @include('layouts.partials.alerts')
