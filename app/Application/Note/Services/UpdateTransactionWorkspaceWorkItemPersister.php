b0d79f53d021f04f08f81da20f4a87c06081c957
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-04-01 16:45:39 +0800

    commit 409

--- app/Application/Note/Services/UpdateTransactionWorkspaceWorkItemPersister.php
+++ app/Application/Note/Services/UpdateTransactionWorkspaceWorkItemPersister.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace App\Application\Note\Services;
 
 use App\Core\Note\Note\Note;
+use App\Ports\Out\Note\WorkItemWriterPort;
 use DateTimeImmutable;
 
 final class UpdateTransactionWorkspaceWorkItemPersister
 {
     public function __construct(
         private readonly ReverseIssuedInventoryByNoteService $reverseIssuedInventoryByNote,
+        private readonly WorkItemWriterPort $workItems,
         private readonly CreateTransactionWorkspaceWorkItemPersister $createPersister,
     ) {
     }
@@ -21,7 +23,7 @@ final class UpdateTransactionWorkspaceWorkItemPersister
     public function persist(Note $note, mixed $items, DateTimeImmutable $date): int
     {
         $this->reverseIssuedInventoryByNote->execute($note, $date);
-        $this->createPersister->workItems->deleteByNoteId($note->id());
+        $this->workItems->deleteByNoteId($note->id());
 
         $note->replaceWorkItems([]);
 
