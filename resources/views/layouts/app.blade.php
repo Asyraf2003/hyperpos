bbec33b287bda3d600657db2706af22b5b5f846e
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-04-04 15:11:13 +0800

    commit 494

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,6 +61,7 @@
     <script src="{{ asset('assets/extensions/flatpickr/flatpickr.js') }}?v={{ filemtime(public_path('assets/extensions/flatpickr/flatpickr.js')) }}"></script>
     <script src="{{ asset('assets/extensions/flatpickr/l10n/id.js') }}?v={{ filemtime(public_path('assets/extensions/flatpickr/l10n/id.js')) }}"></script>
     <script src="{{ asset('assets/static/js/shared/admin-date-input.js') }}?v={{ filemtime(public_path('assets/static/js/shared/admin-date-input.js')) }}"></script>
+    <script src="{{ asset('assets/static/js/shared/page-freshness.js') }}?v={{ filemtime(public_path('assets/static/js/shared/page-freshness.js')) }}"></script>
     @stack('scripts')
 </body>
 </html>
