9bf40795746b6c7d9fec71b9af212ec6ebbaeb0d
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-04-08 07:37:37 +0800

    commit 574

--- app/Adapters/In/Http/Controllers/Admin/Product/ShowProductPageController.php
+++ app/Adapters/In/Http/Controllers/Admin/Product/ShowProductPageController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Adapters\In\Http\Controllers\Admin\Product;
 
+use App\Adapters\In\Http\Presenters\Admin\Product\ProductDetailPagePresenter;
 use App\Application\ProductCatalog\UseCases\GetProductDetailHandler;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
@@ -13,6 +14,7 @@ final class ShowProductPageController extends Controller
 {
     public function __invoke(
         GetProductDetailHandler $useCase,
+        ProductDetailPagePresenter $presenter,
         string $productId,
     ): View|RedirectResponse {
         $result = $useCase->handle($productId);
@@ -24,8 +26,7 @@ final class ShowProductPageController extends Controller
         }
 
         return view('admin.products.show', [
-            'detail' => $result->data()['detail'],
-            'timeline' => $result->data()['timeline'],
+            'page' => $presenter->present($result->data()),
         ]);
     }
 }
