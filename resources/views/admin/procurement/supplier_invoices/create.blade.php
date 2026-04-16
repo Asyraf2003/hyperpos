65d93320f6b39cab74162d5198ac7770ab0f920f
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-04-16 18:54:00 +0800

    commit 852

--- resources/views/admin/procurement/supplier_invoices/create.blade.php
+++ resources/views/admin/procurement/supplier_invoices/create.blade.php
@@ -404,10 +404,11 @@
             lookupEndpoint: @json(route('admin.procurement.products.lookup')),
             supplierLookupEndpoint: @json(route('admin.procurement.suppliers.lookup')),
             clearDraftOnLoad: @json((bool) session('clear_procurement_create_draft')),
-            createProductUrl: @json(route('admin.products.create', [
-                'return_to' => route('admin.procurement.supplier-invoices.create'),
-                'return_label' => 'Kembali ke Nota Pemasok',
-            ]))
+            createProductUrl: @json(
+                route('admin.products.create')
+                    . '?return_to=' . urlencode(route('admin.procurement.supplier-invoices.create'))
+                    . '&return_label=' . urlencode('Kembali ke Nota Pemasok')
+            )
         };
     </script>
     <script src="{{ asset('assets/static/js/pages/admin-procurement-create.js') }}"></script>
