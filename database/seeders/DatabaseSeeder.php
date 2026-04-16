f5f673d24bfec555133f93931c8143775921d083
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-04-17 06:54:36 +0800

    commit 905

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             ProductSeeder::class,
+            ProductInventoryThresholdBackfillSeeder::class,
             SupplierSeeder::class,
             EmployeeFinanceBaselineSeeder::class,
             SupplierInvoiceScenarioSeeder::class,
