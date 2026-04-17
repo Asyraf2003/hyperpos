8ccb21ef48b30d1033983158d94f9ef44e3d6cf0
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-04-17 09:11:08 +0800

    commit 912

--- app/Adapters/In/Http/Controllers/Admin/AdminDashboardPageController.php
+++ app/Adapters/In/Http/Controllers/Admin/AdminDashboardPageController.php
@@ -4,13 +4,13 @@ declare(strict_types=1);
 
 namespace App\Adapters\In\Http\Controllers\Admin;
 
-use App\Application\Reporting\UseCases\GetAdminDashboardOverviewHandler;
+use App\Application\Reporting\UseCases\GetAdminDashboardPagePayloadHandler;
 use Illuminate\Contracts\View\View;
 use Illuminate\Routing\Controller;
 
 final class AdminDashboardPageController extends Controller
 {
-    public function __invoke(GetAdminDashboardOverviewHandler $useCase): View
+    public function __invoke(GetAdminDashboardPagePayloadHandler $useCase): View
     {
         return view('admin.dashboard.index', [
             'dashboard' => $useCase->handle(),
