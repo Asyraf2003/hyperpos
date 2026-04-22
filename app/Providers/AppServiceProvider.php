fdce644fad79a65de905a0d7ac2fbded1457aa4f
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-04-22 11:50:02 +0800

    commit 1147

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,10 @@
 
 namespace App\Providers;
 
+use App\Adapters\Out\Note\DbNoteRevisionRepository;
 use App\Application\IdentityAccess\Request\IdentityAccessRequestStore;
+use App\Ports\Out\Note\NoteRevisionReaderPort;
+use App\Ports\Out\Note\NoteRevisionWriterPort;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,6 +20,21 @@ class AppServiceProvider extends ServiceProvider
         $this->app->scoped(
             IdentityAccessRequestStore::class,
             fn (): IdentityAccessRequestStore => new IdentityAccessRequestStore()
+        );
+
+        $this->app->scoped(
+            DbNoteRevisionRepository::class,
+            fn (): DbNoteRevisionRepository => new DbNoteRevisionRepository()
+        );
+
+        $this->app->scoped(
+            NoteRevisionReaderPort::class,
+            fn (): NoteRevisionReaderPort => $this->app->make(DbNoteRevisionRepository::class)
+        );
+
+        $this->app->scoped(
+            NoteRevisionWriterPort::class,
+            fn (): NoteRevisionWriterPort => $this->app->make(DbNoteRevisionRepository::class)
         );
     }
 
