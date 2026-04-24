9a263cf5c19bb7d16abf08ab9ef54c6f99ee04d7
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-04-24 20:55:38 +0800

    commit 1343

--- app/Application/Note/Services/NoteDetailPageDataBuilder.php
+++ app/Application/Note/Services/NoteDetailPageDataBuilder.php
@@ -31,7 +31,7 @@ final class NoteDetailPageDataBuilder
         if ($workspacePanel === null) return null;
 
         $operational = $this->operationals->build((array) ($workspacePanel['note_totals'] ?? []));
-        $billingRows = $this->billingProjection->buildFromWorkspaceRows($workspacePanel['rows'] ?? []);
+        $billingRows = $this->billingProjection->build($note->id()) ?? [];
         $history = $this->history->build($note->id());
         $refundOptions = $this->refundPaymentOptions->build($note->id());
         $revisionView = $this->revisionView->build($note);
