451974a75d118a742bddae998cbfdd04c0b2366e
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-04-26 15:41:22 +0800

    commit 1415

--- database/seeders/SupplierSeeder.php
+++ database/seeders/SupplierSeeder.php
@@ -8,45 +8,52 @@ use App\Core\Procurement\Supplier\Supplier;
 use App\Ports\Out\Procurement\SupplierWriterPort;
 use App\Ports\Out\UuidPort;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 final class SupplierSeeder extends Seeder
 {
     public function run(SupplierWriterPort $writer, UuidPort $uuid): void
     {
-        $suppliers = [
-            ['nama' => 'PT. Astra Otoparts (Clutch Div)', 'kontak' => '081234567890'],
-            ['nama' => 'PT. FCC Indonesia', 'kontak' => '081122334455'],
-            ['nama' => 'PT. Exedy Manufacturing Indonesia', 'kontak' => '085566778899'],
-            ['nama' => 'PT. Faito Racing Indonesia', 'kontak' => '081299887766'],
-            ['nama' => 'PT. TDR Industries', 'kontak' => '081311223344'],
-            ['nama' => 'PT. Kawahara Racing', 'kontak' => '085244556677'],
-            ['nama' => 'PT. Bintang Racing Team (BRT)', 'kontak' => '082166778899'],
-            ['nama' => 'PT. Chemco Harapan Nusantara', 'kontak' => '081988776655'],
-            ['nama' => 'PT. Dirgaputra Eta Sembada', 'kontak' => '087855443322'],
-            ['nama' => 'PT. Daido Indonesia Manufacturing', 'kontak' => '081233445566'],
-            ['nama' => 'PT. Federal Izumi Manufacturing', 'kontak' => '081366554433'],
-            ['nama' => 'PT. Showa Indonesia Manufacturing', 'kontak' => '085699887744'],
-            ['nama' => 'PT. Musashi Auto Parts Indonesia', 'kontak' => '081122112233'],
-            ['nama' => 'PT. Nissin Kogyo Indonesia', 'kontak' => '081200998877'],
-            ['nama' => 'PT. Akebono Brake Astra Indonesia', 'kontak' => '082211223344'],
-            ['nama' => 'PT. TPR Indonesia', 'kontak' => '081344332211'],
-            ['nama' => 'PT. Mikuni Indonesia', 'kontak' => '085355667788'],
-            ['nama' => 'PT. Keihin Indonesia', 'kontak' => '081288779900'],
-            ['nama' => 'PT. Denso Indonesia', 'kontak' => '081166554422'],
-            ['nama' => 'PT. Yamaha Indonesia Motor Mfg (Parts)', 'kontak' => '081922334455'],
-            ['nama' => 'PT. Nusantara Motor Parts Sejahtera', 'kontak' => '081700111222'],
-            ['nama' => 'PT. Sinar Teknik Otomotif Abadi', 'kontak' => '081700111333'],
-            ['nama' => 'PT. Prima Sparepart Niaga', 'kontak' => '081700111444'],
-            ['nama' => 'PT. Cipta Komponen Kendara', 'kontak' => '081700111555'],
-            ['nama' => 'PT. Sentra Distribusi Partindo', 'kontak' => '081700111666'],
+        $supplierNames = [
+            'PT. Astra Otoparts (Clutch Div)',
+            'PT. FCC Indonesia',
+            'PT. Exedy Manufacturing Indonesia',
+            'PT. Faito Racing Indonesia',
+            'PT. TDR Industries',
+            'PT. Kawahara Racing',
+            'PT. Bintang Racing Team (BRT)',
+            'PT. Chemco Harapan Nusantara',
+            'PT. Dirgaputra Eta Sembada',
+            'PT. Daido Indonesia Manufacturing',
+            'PT. Federal Izumi Manufacturing',
+            'PT. Showa Indonesia Manufacturing',
+            'PT. Musashi Auto Parts Indonesia',
+            'PT. Nissin Kogyo Indonesia',
+            'PT. Akebono Brake Astra Indonesia',
+            'PT. TPR Indonesia',
+            'PT. Mikuni Indonesia',
+            'PT. Keihin Indonesia',
+            'PT. Denso Indonesia',
+            'PT. Yamaha Indonesia Motor Mfg (Parts)',
+            'PT. Nusantara Motor Parts Sejahtera',
+            'PT. Sinar Teknik Otomotif Abadi',
+            'PT. Prima Sparepart Niaga',
+            'PT. Cipta Komponen Kendara',
+            'PT. Sentra Distribusi Partindo',
         ];
 
-        foreach ($suppliers as $s) {
-            $supplier = Supplier::create(
-                $uuid->generate(),
-                $s['nama'],
-                $s['kontak']
-            );
+        foreach ($supplierNames as $supplierName) {
+            $supplier = Supplier::create($uuid->generate(), $supplierName);
+
+            $exists = DB::table('suppliers')
+                ->where('nama_pt_pengirim_normalized', $supplier->namaPtPengirimNormalized())
+                ->whereNull('deleted_at')
+                ->exists();
+
+            if ($exists) {
+                continue;
+            }
+
             $writer->create($supplier);
         }
     }
