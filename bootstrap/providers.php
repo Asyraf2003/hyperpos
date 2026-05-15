c1ac8669a0576f0ee05c83b7ae070b053ba67353
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-05-15 23:01:08 +0800

    commit 2140

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    App\Providers\AppServiceProvider::class,
     App\Providers\IdentityAccessServiceProvider::class,
     App\Providers\NoteRevisionServiceProvider::class,
     App\Providers\MobileApiServiceProvider::class,
