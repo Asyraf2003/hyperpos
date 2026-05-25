9c6651b34580b4918e6eafc07ea9fb8d977eaf12
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-05-25 11:58:48 +0800

    commit 2300

--- app/Application/Reporting/Exports/TransactionCashLedgerExcelSummarySheetWriter.php
+++ app/Application/Reporting/Exports/TransactionCashLedgerExcelSummarySheetWriter.php
@@ -25,6 +25,8 @@ final class TransactionCashLedgerExcelSummarySheetWriter
         $this->tables->writeTable($sheet, 5, ['Metrik', 'Nilai'], [
             ['Total Kejadian', (int) ($summary['total_events'] ?? 0)],
             ['Kas Masuk', (int) ($summary['total_cash_in_rupiah'] ?? 0)],
+            ['Tunai Masuk', (int) ($summary['cash_in_rupiah'] ?? 0)],
+            ['Transfer Masuk', (int) ($summary['transfer_in_rupiah'] ?? 0)],
             ['Kas Keluar', (int) ($summary['total_cash_out_rupiah'] ?? 0)],
             ['Nilai Bersih', (int) ($summary['net_amount_rupiah'] ?? 0)],
         ]);
