50c65ed77cebc34782bf64417bdcbc646734d0dc
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-06-01 03:48:03 +0800

    commit 2492

--- resources/views/cashier/notes/partials/note-row-package-breakdown.blade.php
+++ resources/views/cashier/notes/partials/note-row-package-breakdown.blade.php
@@ -1,25 +1,19 @@
-@php
-  $packageBreakdown = is_array($row['package_breakdown'] ?? null)
-    ? $row['package_breakdown']
-    : null;
-@endphp
-
-@if ($packageBreakdown !== null)
+@if (is_array($row['package_breakdown'] ?? null))
   <div class="small text-muted mt-2">
     <div>
       Paket total:
-      Rp {{ number_format((int) ($packageBreakdown['package_total_rupiah'] ?? 0), 0, ',', '.') }}
+      Rp {{ number_format((int) ($row['package_breakdown']['package_total_rupiah'] ?? 0), 0, ',', '.') }}
     </div>
     <div>
       Total sparepart:
-      Rp {{ number_format((int) ($packageBreakdown['parts_total_rupiah'] ?? 0), 0, ',', '.') }}
+      Rp {{ number_format((int) ($row['package_breakdown']['parts_total_rupiah'] ?? 0), 0, ',', '.') }}
     </div>
     <div>
       Sisa jasa:
-      Rp {{ number_format((int) ($packageBreakdown['service_residual_rupiah'] ?? 0), 0, ',', '.') }}
+      Rp {{ number_format((int) ($row['package_breakdown']['service_residual_rupiah'] ?? 0), 0, ',', '.') }}
     </div>
 
-    @foreach (($packageBreakdown['parts'] ?? []) as $part)
+    @foreach (($row['package_breakdown']['parts'] ?? []) as $part)
       <div>
         - {{ $part['product_name'] ?? $part['product_id'] ?? '-' }}
         x{{ (int) ($part['qty'] ?? 0) }}
