e196ed748b9bf8ef517d52d9d03396aa5031663c
Asyraf2003 <asyrafmubarak738@gmail.com>
2026-06-23 00:40:45 +0800

    commit 2958

--- tests/Feature/Inventory/RebuildInventoryCostingProjectionWithStockOutFeatureTest.php
+++ tests/Feature/Inventory/RebuildInventoryCostingProjectionWithStockOutFeatureTest.php
@@ -15,6 +15,46 @@ final class RebuildInventoryCostingProjectionWithStockOutFeatureTest extends Tes
     use RefreshDatabase;
     use SeedsMinimalInventoryProductFixture;
 
+    public function test_rebuild_costing_projection_handles_value_only_cost_revaluation(): void
+    {
+        $this->seedInventoryProduct('product-1', 'KB-001', 'Ban Luar', 'Federal', 100, 12000);
+
+        DB::table('inventory_movements')->insert([
+            [
+                'id' => 'm1',
+                'product_id' => 'product-1',
+                'movement_type' => 'stock_in',
+                'source_type' => 'supplier_receipt_line',
+                'source_id' => 'receipt-line-1',
+                'tanggal_mutasi' => '2026-03-16',
+                'qty_delta' => 2,
+                'unit_cost_rupiah' => 10000,
+                'total_cost_rupiah' => 20000,
+            ],
+            [
+                'id' => 'm2',
+                'product_id' => 'product-1',
+                'movement_type' => 'cost_revaluation',
+                'source_type' => 'supplier_invoice_cost_revaluation',
+                'source_id' => 'invoice-line-2',
+                'tanggal_mutasi' => '2026-03-17',
+                'qty_delta' => 0,
+                'unit_cost_rupiah' => 0,
+                'total_cost_rupiah' => 2000,
+            ],
+        ]);
+
+        $handler = app(RebuildInventoryCostingProjectionHandler::class);
+
+        $handler->handle();
+
+        $this->assertDatabaseHas('product_inventory_costing', [
+            'product_id' => 'product-1',
+            'avg_cost_rupiah' => 11000,
+            'inventory_value_rupiah' => 22000,
+        ]);
+    }
+
     public function test_rebuild_costing_projection_handles_stock_out(): void
     {
         $this->seedInventoryProduct('product-1', 'KB-001', 'Ban Luar', 'Federal', 100, 12000);
